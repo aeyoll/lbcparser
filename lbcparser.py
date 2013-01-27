0df5960f31804de98c94867ae8a01fa4a28d01fc
Jean-Philippe Bidegain <jp@bidega.in>
2013-01-27 18:21:37 +0100

    Ajout d'un formulaire pour saisir l'url ) requêter

--- lbcparser.py
+++ lbcparser.py
@@ -4,13 +4,22 @@ from view import render
 
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/url', 'url'
 )
 
 
 class index:
     def GET(self):
-        return render.base(view.listing())
+        return render.base(view.form())
+
+
+class url:
+    def GET(self):
+        # return "%s %s\n" % (name, url)
+        form = web.input()
+        url = form.url
+        return render.base(view.listing(url))
 
 
 if __name__ == "__main__":
---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+// $(document).ready(function() {
+// 	$('#submit_url').on('submit', function() {
+// 		var url = $(this).attr('action');
+// 		$(this).attr('action', url+'/'+$('#url').val());
+// 	});
+// });
